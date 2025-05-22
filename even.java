b59d4068cdfea7806eaa6189b4a449cf26f2e605
sushil7755 <kendresushil6@gmail.com>
2025-05-22 21:59:47 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,10 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        System.out.println("Even numbers from 0 to 40:");
+        for (int i = 0; i <= 40; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
