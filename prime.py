46eddaccdc6d52add5392fc21f796c88ff568b49
sushil7755 <kendresushil6@gmail.com>
2025-05-22 21:38:52 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,22 @@
+# prime_numbers.py
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def first_n_primes(count):
+    primes = []
+    num = 2
+    while len(primes) < count:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+# Print the first 10 prime numbers
+for prime in first_n_primes(10):
+    print(prime)
